71bf6babca961304b08d6124637386bcd03200d7
李喆 <lizhe@Zhes-MacBook-Pro.local>
2018-12-20 03:37:18 +0100

    test firebase connection

--- app/src/main/java/com/example/natalia/diary_lmu/DiaryContentActivity.java
+++ app/src/main/java/com/example/natalia/diary_lmu/DiaryContentActivity.java
@@ -1,19 +1,26 @@
 package com.example.natalia.diary_lmu;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnPausedListener;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageException;
-import com.google.firebase.storage.StorageMetadata;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.StorageReference;
 
 public class DiaryContentActivity extends AppCompatActivity {
 
@@ -24,51 +31,32 @@ public class DiaryContentActivity extends AppCompatActivity {
 
         Bundle bundle = this.getIntent().getExtras();
         String name = bundle.getString("name");
-//        Toast toast = Toast.makeText(getApplicationContext(),
-//                        "name is " + name,
-//                        Toast.LENGTH_SHORT);
-//        toast.show();
 
-    }
+        final ImageView testImageView = findViewById(R.id.firebaseTestImageView);
 
-    private void testFirebase(){
-        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        storageRef.child("test_folder/test_image.jpeg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(testImageView);
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "load image successed",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "load image failed" + e,
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
-        // ## Create a Reference
+//        GlideApp.with(this /* context */)
+//                .load(storageRef)
+//                .into(findViewById(R.id.firebaseTestImageView));
 
-        // [START create_storage_reference]
-        // Create a storage reference from our app
-        StorageReference storageRef = storage.getReference();
-        // Points to the root reference
-        storageRef = storage.getReference();
-
-        // [END create_storage_reference]
-
-        // [START create_child_reference]
-        // Create a child reference
-        // imagesRef now points to "images"
-        StorageReference imagesRef = storageRef.child("images");
-// Points to "images"
-        imagesRef = storageRef.child("images");
-
-        // Child references can also take paths
-        // spaceRef now points to "images/space.jpg
-        // imagesRef still points to "images"
-        StorageReference spaceRef = storageRef.child("images/space.jpg");
-        // [END create_child_reference]
-
-// Points to "images/space.jpg"
-// Note that you can use variables to create child values
-        String fileName = "space.jpg";
-        spaceRef = imagesRef.child(fileName);
-
-// File path is "images/space.jpg"
-        String path = spaceRef.getPath();
-
-// File name is "space.jpg"
-        String name = spaceRef.getName();
-
-// Points to "images"
-        imagesRef = spaceRef.getParent();
     }
 }
