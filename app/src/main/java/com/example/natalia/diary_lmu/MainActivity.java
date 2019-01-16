0c8e831d6507e47ff4d25d85406a34bf1dde411a
natalia <lomonat@gmail.com>
2019-01-16 19:54:50 +0100

    response

--- app/src/main/java/com/example/natalia/diary_lmu/MainActivity.java
+++ app/src/main/java/com/example/natalia/diary_lmu/MainActivity.java
@@ -4,10 +4,36 @@ import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String REQUEST_METHOD = "GET";
+    public static final int READ_TIMEOUT = 15000;
+    public static final int CONNECTION_TIMEOUT = 15000;
 
 
     @Override
@@ -22,10 +48,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onStart() {
         super.onStart();
+
         // Check if user is signed in (non-null) and update UI accordingly.
+        try {
+            getData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
+    public void getData() throws IOException {
+        System.out.println("test2");
+
+
+       // URL url = new URL("https://api.telegram.org/bot594094944:AAHr-ucsrdRrS5wQraiHmGgog2vyCZ_9XXk/getUpdates");
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.telegram.org/bot594094944:AAHr-ucsrdRrS5wQraiHmGgog2vyCZ_9XXk/getUpdates";
+
+// Request a string response from the provided URL.
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, (String) null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error.Response", "test", error);
+                    }
+                }
+        );
+            // add it to the RequestQueue
+                    queue.add(getRequest);
+
+
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
