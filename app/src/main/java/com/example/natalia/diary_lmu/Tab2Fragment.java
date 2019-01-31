277ca317a4200a0f9129200a4b2e23502692115e
李喆 <lizhe@dhcp-10-176-90-6.dynamic.eduroam.mwn.de>
2019-01-31 23:01:05 +0100

    Change Activity to Fragment for whole project

---
+++ app/src/main/java/com/example/natalia/diary_lmu/Tab2Fragment.java
@@ -0,0 +1,15 @@
+package com.example.natalia.diary_lmu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
+    }
+}
