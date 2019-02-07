dae778597997cde7285a9d12fb46169f113322e4
李喆 <lizhe@dhcp-10-181-45-31.dynamic.eduroam.mwn.de>
2019-02-07 19:21:33 +0100

    change label to username

--- app/src/main/java/com/example/natalia/diary_lmu/Tab3Fragment.java
+++ app/src/main/java/com/example/natalia/diary_lmu/Tab3Fragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
